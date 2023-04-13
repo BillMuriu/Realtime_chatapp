92c8bcc3ef5bd791c76ac210954e8c982e980267
unknown <njorogekamau63@gmail.com>
2023-04-13 21:46:02 +0300

    add a post form in the index.html file

--- ChatApp/Chat_App/views.py
+++ ChatApp/Chat_App/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room, Message
 
 # Create your views here.
 
 def HomePage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        room = request.POST['room']
+
+        try:
+            get_room = Room.objects.get(room_name=room)
+            return redirect('room', room_name=room, username=username)
+
+        except Room.DoesNotExist:
+            new_room = Room(room_name = room)
+            new_room.save()
+            return redirect('room', room_name=room, username=username)
+
     return render(request, 'index.html')
+
